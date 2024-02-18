bda4d7740bbf23f8b130bdf6ba6102bdff346d85
Sasha Plukchi <loxpojizniia@gmail.com>
2024-02-18 15:07:25 +0200

    Добавил Ввод массива строк с клавиатуры

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строки через запятую:");
+        string input = Console.ReadLine()!;
+        string[] arr = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Далее будем использовать arr для дальнейших операций
+    }
+}
